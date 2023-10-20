75740d41cd6a2245a784c1a91635db2d92479fac
AlePatata <alejandra.campos.u@ug.uchile.cl>
2023-10-19 21:38:26 -0300

    Se agrega el mini proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Fragment, useState} from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Producto from './components/Producto';
+import Carrito from './components/Carrito';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [ productos, guardarProductos ] = useState([
+        {id: 1, nombre: 'Camisa ReactJS', precio: 50},
+        {id: 2, nombre: 'Camisa ReactJS2', precio: 50},
+        {id: 3, nombre: 'Camisa ReactJS3', precio: 50},
+        {id: 4, nombre: 'Camisa ReactJS4', precio: 50},
+    ]);
+
+    const [ carrito, agregarProducto] =useState([]);
+
+    const date = new Date().getFullYear();
+
+    return (
+        <Fragment>
+            <Header
+                titulo = 'Tienda Virtual'
+            />
+
+            <h1>Lista de Productos</h1>
+            {productos.map(producto => (
+                <Producto
+                    key={producto.id}
+                    producto={producto}
+                    productos={productos}
+                    carrito={carrito}
+                    agregarProducto={agregarProducto}
+                />
+            ))}
+
+            <Carrito
+                carrito={carrito}
+                agregarProducto={agregarProducto}
+            />
+
+            <Footer />
+              date = {date}
+        </Fragment>
+    )
 }
 
 export default App;
---
+++ src/serviceWorker.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function serviceWorker() {
+
+}
+export default serviceWorker()
